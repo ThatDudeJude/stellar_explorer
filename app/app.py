14c1bf8dd791f4925fecfa3b43425de698c59bcd
ThatDudeJude <judegachoki@gmail.com>
2026-05-12 17:18:01 +0300

    Added CMD 2D Histogram Contour plot

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 from utils import load_fits, add_galactic_coords, add_abs_mag, add_distance_pc
 
@@ -167,8 +168,125 @@ with sky_map_tab:
     else:
         st.write("To view the sky plot, select data file or fetch data to load from Data Source.")
 
+# CMD Tab
 with cmd_tab:
     st.header("Colour-Magnitude Diagram")    
+        
+    # Update loaded data and check for coordinate type
+    if data_source == 'File Upload' and source_is_loaded:
+        
+        # Add Galactic Coords, Absolute Magnitude and Distance columns to dataframe
+        
+        cmd_sources_df = add_galactic_coords(sources_df)
+        cmd_sources_df = add_abs_mag(cmd_sources_df)
+        cmd_sources_df = add_distance_pc(cmd_sources_df)
+        
+        # Create new abs_mag_log column for log-stetch normalization
+        cmd_sources_df['abs_mag_log'] = np.log10(cmd_sources_df['abs_mag'])
+        
+        # Rename columns for hover data 
+        
+        cmd_sources_df = cmd_sources_df.rename(columns={'source_id': 'source id', 'phot_g_mean_mag': 'G mag', 'abs_mag_log': 'log(G mag)', 'bp_rp': 'BP-RP', 
+                                                                'distance_pc': 'distance (pc)', 'abs_mag': 'M (G-band)', 'gal_l': 'gal l', 'gal_b': 'gal b'})
+        
+        col1, col2 = st.columns(2)
+        
+        # Add radio buttons for selecting between scatter and histogram 2d contour plot        
+        with col1:
+            plot_selected = st.radio('Plot Type', ['Scatter', '2D Histogram Contour'])
+            
+            
+        
+        # Select radio buttons displayed in col 2 based on plot type
+        with col2:
+            #  Scatter plot
+            if plot_selected == 'Scatter':
+                # Add radio buttons for selecting colour mapping based on parallax, distance, absolute magnitude or none
+                
+                colour_map_var = st.radio("Variable for Colour Points", ['None', 'Parallax (mas)', 'Distance (pc)', 'G (mag)'])
+                                
+                # Update related arguments based on selection        
+                if colour_map_var == 'None':
+                    range_colour = [0, 0]
+                    var_selected = None
+                elif colour_map_var == 'Parallax (mas)':
+                    range_colour = [cmd_sources_df['parallax'].min(), cmd_sources_df['parallax'].max()]
+                    var_selected = 'parallax'
+                elif colour_map_var == 'Distance (pc)':
+                    range_colour = [cmd_sources_df['distance (pc)'].min(), cmd_sources_df['distance (pc)'].max()]
+                    var_selected = 'distance (pc)'
+                elif colour_map_var == 'G (mag)':
+                    range_colour = [cmd_sources_df['G mag'].min(), cmd_sources_df['G mag'].max()]
+                    var_selected = 'G mag'
+            
+            
+            # 2D Histogram Contour
+            elif plot_selected == '2D Histogram Contour':
+                # Add radio buttons for selecting binning function
+                
+                bin_func = st.radio("Choose Binning Function", ['Count (stellar density)', 'Average (average distance per bin)'])
+                
+                if bin_func == 'Count (stellar density)':
+                    hist_func = 'count' 
+                    z_col = None
+                    cb_title = 'Stars per bin'
+                    hover_temp_stat = 'Count'
+                elif bin_func == 'Average (average distance per bin)':
+                    hist_func = 'avg'
+                    z_col = cmd_sources_df['parallax']
+                    cb_title = 'Average distance per bin'
+                    hover_temp_stat = 'Avg Distance:'
+        
+        #  Plot CMD
+        
+        if plot_selected == 'Scatter':            
+            # Scatter Plot
+            
+            fig = px.scatter(
+                cmd_sources_df,
+                x='BP-RP',
+                y='G mag',
+                color=var_selected,
+                range_color=range_colour,
+                hover_data={'source id': True, 'G mag': ':.2f', 'BP-RP': ':.2f', 'parallax': ':.2f', 'distance (pc)': ':.1f', 'log(G mag)': False, 'M (G-band)': ':.2f'},
+                labels={'BP-RP': 'BP - RP (mag)', 'G mag': 'G (mag)'},
+                title=f'Colour Magnitude Diagram (N={len(sky_map_sources_df)} sources)'
+            )
+                        
+            
+        elif plot_selected == '2D Histogram Contour':
+            # 2D Histogram Contour
+            
+            # Pick a binning number for both x and y
+            xbins = st.slider("Number of bins for x axis", 2, 100)
+            ybins = st.slider("Number of bins for y axis", 2, 100)
+            
+            fig = go.Figure(
+                go.Histogram2dContour(                    
+                    x=cmd_sources_df['BP-RP'],
+                    y=cmd_sources_df['G mag'],
+                    z=z_col,
+                    histfunc=hist_func,
+                    nbinsx=xbins,
+                    nbinsy=ybins,
+                    contours=dict(
+                        coloring='heatmap',
+                        showlabels=False,                        
+                    ),
+                    colorbar=dict(
+                        title=cb_title, 
+                        tickformat='.0f'
+                    ),
+                    hovertemplate='Colour: %{x:.2f}<br>Magnitude: %{y:.2f}<br>' + hover_temp_stat + ' %{z:.0f}<extra></extra>',                    
+                ),                
+            )
+        fig.update_yaxes(autorange="reversed")
+        
+        
+        st.plotly_chart(fig, width='stretch')
+            
+    else:
+        st.write("To view the Colour Magnitude Diagram, select data file or fetch data to load from Data Source.")
     
 with hr_tab:
     st.header("Hertzsprung-Russel Diagram")
